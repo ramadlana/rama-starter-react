c4d503b27f73ef6224ebe775982fa06599d3beb6
ramadlana <hidayahweb@gmail.com>
2021-10-25 22:32:37 +0700

    pagination search completed

--- src/components/Persons.jsx
+++ src/components/Persons.jsx
@@ -3,11 +3,20 @@ import _ from "lodash";
 import React from "react";
 import Table from "./common/Table";
 
+// params for Axios
+const apiHost = "http://localhost:8000";
+const apiEndpoint = "/example/show-person-division-searching-sort-pagination";
+const apiToken =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTczYzZlOGU0ODJiZDJjNjhjMWRlOGEiLCJ1c2VybmFtZSI6ImFkbWluIiwiZW1haWwiOiJhZG1pbkBnbWFpbC5jb20iLCJpc0FjdGl2ZSI6dHJ1ZSwiaXNBZG1pbiI6dHJ1ZSwiaWF0IjoxNjM0OTc3NTk1LCJleHAiOjE2NjY1MTM1OTV9.D9N49GIXXySANYe3vgHqEkfTX4dH2KdtXb6gNIg0IMg";
+
 class Person extends React.Component {
   state = {
     tableRowsData: [],
+    searchBy: "name",
+    searchQuery: "",
     maxPerPage: 10,
     currentPage: 1,
+    sortBy: "",
   };
 
   tableColumnData = [
@@ -15,25 +24,30 @@ class Person extends React.Component {
       id: "id-col",
       columnName: "Name",
       path: "name",
+      searchAble: true,
     },
     {
       id: "id-add",
       columnName: "Address",
       path: "address",
+      searchAble: true,
     },
     {
       id: "id-phone",
       columnName: "Phone",
       path: "phone",
+      searchAble: true,
     },
     {
       id: "id-div",
       columnName: "Division Name",
       path: "divisionId.divisionName",
+      searchAble: false,
     },
     {
       id: "id-act",
       columnName: "Action",
+      searchAble: false,
       componentAsPath: (data) => (
         <div className="btn-group btn-group-sm">
           <button
@@ -58,6 +72,29 @@ class Person extends React.Component {
     },
   ];
 
+  // Axios Call Function Block
+  axiosParams = {
+    method: "POST",
+    url: apiHost + apiEndpoint,
+    headers: {
+      "Content-Type": "application/json",
+      "x-auth-token": apiToken,
+    },
+    data: {
+      searchBy: this.state.searchBy,
+      searchQuery: this.state.searchQuery,
+      currentPage: this.state.currentPage,
+      maxPerPage: this.state.maxPerPage,
+      sortBy: this.sortBy,
+    },
+  };
+
+  async axiosCall() {
+    const { data } = await axios.request(this.axiosParams);
+    return data.message.data;
+  }
+
+  // START REUSABLE FUNC
   // Handling Event
   handleDelete = (data) => {
     const copytableRowsData = [...this.state.tableRowsData];
@@ -65,39 +102,47 @@ class Person extends React.Component {
     this.setState({ tableRowsData: copytableRowsData });
   };
 
-  handleChange = async (e) => {
-    const value = e.target.value;
-    const options = {
-      method: "GET",
-      url:
-        "http://localhost:8000/example/show-person-division-pagination/" +
-        `${this.state.currentPage}/${value}`,
-      headers: {
-        "Content-Type": "application/json",
-        "x-auth-token":
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTczYzZlOGU0ODJiZDJjNjhjMWRlOGEiLCJ1c2VybmFtZSI6ImFkbWluIiwiZW1haWwiOiJhZG1pbkBnbWFpbC5jb20iLCJpc0FjdGl2ZSI6dHJ1ZSwiaXNBZG1pbiI6dHJ1ZSwiaWF0IjoxNjM0OTc3NTk1LCJleHAiOjE2NjY1MTM1OTV9.D9N49GIXXySANYe3vgHqEkfTX4dH2KdtXb6gNIg0IMg",
-      },
-    };
-
-    const { data } = await axios.request(options);
-    this.setState({ tableRowsData: data.message.data });
+  handleMaxPerPage = async (e) => {
+    const value = e;
+    this.setState({ maxPerPage: value });
+    this.axiosParams.data.maxPerPage = value;
+    const newData = await this.axiosCall();
+    this.setState({ tableRowsData: newData });
   };
 
-  async componentDidMount() {
-    const options = {
-      method: "GET",
-      url:
-        "http://localhost:8000/example/show-person-division-pagination/" +
-        `${this.state.currentPage}/${this.state.maxPerPage}`,
-      headers: {
-        "Content-Type": "application/json",
-        "x-auth-token":
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTczYzZlOGU0ODJiZDJjNjhjMWRlOGEiLCJ1c2VybmFtZSI6ImFkbWluIiwiZW1haWwiOiJhZG1pbkBnbWFpbC5jb20iLCJpc0FjdGl2ZSI6dHJ1ZSwiaXNBZG1pbiI6dHJ1ZSwiaWF0IjoxNjM0OTc3NTk1LCJleHAiOjE2NjY1MTM1OTV9.D9N49GIXXySANYe3vgHqEkfTX4dH2KdtXb6gNIg0IMg",
-      },
-    };
+  handlePagination = async (cur) => {
+    const valCur = parseInt(cur);
+    let copyState = { ...this.state };
+    copyState.currentPage += valCur;
+    // Reset to page 1 if current page state is bellow 0
+    if (copyState.currentPage <= 0) copyState.currentPage = 1;
+    // set state cur page
+    this.setState({ currentPage: copyState.currentPage });
+    // assign axios params with new value
+    this.axiosParams.data.currentPage = copyState.currentPage;
+    // call server using modified axios params
+    const newData = await this.axiosCall();
+    // push to new dataset table
+    this.setState({ tableRowsData: newData });
+  };
 
-    const { data } = await axios.request(options);
-    this.setState({ tableRowsData: data.message.data });
+  handleSearch = async (inputName, val) => {
+    if (inputName == "searchBy") this.axiosParams.data.searchBy = val;
+    this.setState({ [inputName]: val });
+    if (inputName == "searchQuery") {
+      this.axiosParams.data.searchQuery = val;
+
+      // call server using modified axios params
+      const newData = await this.axiosCall();
+      // push to new dataset table
+      this.setState({ tableRowsData: newData });
+    }
+  };
+
+  // Load Init Table Data Component did Mount
+  async componentDidMount() {
+    const dataTable = await this.axiosCall();
+    this.setState({ tableRowsData: dataTable });
   }
 
   render() {
@@ -114,7 +159,10 @@ class Person extends React.Component {
               keyForTableRows="_id"
               tableColumn={this.tableColumnData}
               tableRows={this.state.tableRowsData}
-              handleChange={this.handleChange}
+              maxPerPage={this.state.maxPerPage}
+              handleMaxPerPage={this.handleMaxPerPage}
+              handlePagination={this.handlePagination}
+              handleSearch={this.handleSearch}
             ></Table>
           </div>
         </div>
--- src/components/common/Table.jsx
+++ src/components/common/Table.jsx
@@ -1,71 +1,168 @@
+import React from "react";
 import _ from "lodash";
-const Table = ({ keyForTableRows, tableRows, tableColumn, handleChange }) => {
-  const renderCell = (data, col) => {
+
+class Table extends React.Component {
+  renderCell = (data, col) => {
     // if componentAspath available, return component as path
     if (col.componentAsPath) return col.componentAsPath(data);
     // if not, path retruned using lodash get
     return _.get(data, col.path);
   };
 
-  return (
-    <div>
-      <div className="d-flex">
-        <div className="text-muted">
-          Show
-          <div className="mx-2 d-inline-block">
-            <input
-              type="number"
-              name="maxPerPage"
-              className="form-control form-control-sm"
-              defaultValue={10}
-              size={1}
-              aria-label="count per Page"
-              onChange={(e) => handleChange(e)}
-            />
+  render() {
+    const {
+      keyForTableRows,
+      tableRows,
+      tableColumn,
+      handleMaxPerPage,
+      handlePagination,
+      handleSearch,
+    } = this.props;
+    return (
+      <div>
+        <div className="d-flex">
+          <div className="text-muted">
+            Show
+            <div className="ms-2 d-inline-block">
+              <select
+                onChange={(e) => handleMaxPerPage(e.currentTarget.value)}
+                className="form-select form-select-sm"
+              >
+                <option value={10}>10</option>
+                <option value={25}>25</option>
+                <option value={50}>50</option>
+                <option value={100}>100</option>
+                <option value={200}>200</option>
+              </select>
+            </div>
+            entries
           </div>
-          entries
+
+          <div className="ms-auto text-muted md-2">
+            Search By:
+            <div className="ms-2 d-inline-block">
+              <select
+                name="searchBy"
+                onChange={(e) =>
+                  handleSearch(e.currentTarget.name, e.currentTarget.value)
+                }
+                className="form-select form-select-sm"
+              >
+                {tableColumn.map((col) => {
+                  if (col.searchAble)
+                    return (
+                      <option key={col.id} value={col.path}>
+                        {col.columnName}
+                      </option>
+                    );
+                })}
+              </select>
+            </div>
+            <div className="ms-2 d-inline-block">
+              <input
+                type="text"
+                name="searchQuery"
+                placeholder="text to search here...."
+                className="form-control form-control-sm"
+                aria-label="Search invoice"
+                onChange={(e) =>
+                  handleSearch(e.currentTarget.name, e.currentTarget.value)
+                }
+              />
+            </div>
+          </div>
         </div>
 
-        <div className="ms-auto text-muted md-2">
-          Search By:
-          <div className="ms-2 d-inline-block">
-            <select className="form-select form-select-sm">
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
-            </select>
-          </div>
-          <div className="ms-2 d-inline-block">
-            <input
-              type="text"
-              placeholder="text to search here...."
-              className="form-control form-control-sm"
-              aria-label="Search invoice"
-            />
-          </div>
+        <table className="table table-striped table-sm mt-3">
+          <thead>
+            <tr>
+              {tableColumn.map((col) => {
+                return <th key={col.id}>{col.columnName}</th>;
+              })}
+            </tr>
+          </thead>
+          <tbody>
+            {tableRows.map((data) => (
+              <tr key={data[keyForTableRows]}>
+                {tableColumn.map((col) => (
+                  <td key={col.id}>{this.renderCell(data, col)}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div className="card-footer d-flex align-items-center">
+          <p className="m-0 text-muted">
+            Showing <span>1</span> to <span>8</span> of <span>16</span> entries
+          </p>
+          <ul className="pagination m-0 ms-auto">
+            <li className="page-item">
+              <button
+                className="page-link"
+                value={-1}
+                onClick={(e) => handlePagination(e.currentTarget.value)}
+                tabIndex={-1}
+                aria-disabled="true"
+              >
+                {/* Download SVG icon from http://tabler-icons.io/i/chevron-left */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="icon"
+                  width={24}
+                  height={24}
+                  viewBox="0 0 24 24"
+                  strokeWidth={2}
+                  stroke="currentColor"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <polyline points="15 6 9 12 15 18" />
+                </svg>
+                prev
+              </button>
+            </li>
+            <li className="page-item active">
+              <button value={1} className="page-link">
+                1
+              </button>
+            </li>
+            <li className="page-item">
+              <button value={2} className="page-link">
+                2
+              </button>
+            </li>
+            <li className="page-item">
+              <button
+                className="page-link"
+                value={1}
+                onClick={(e) => handlePagination(e.currentTarget.value)}
+              >
+                next{" "}
+                {/* Download SVG icon from http://tabler-icons.io/i/chevron-right */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="icon"
+                  width={24}
+                  height={24}
+                  viewBox="0 0 24 24"
+                  strokeWidth={2}
+                  stroke="currentColor"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <polyline points="9 6 15 12 9 18" />
+                </svg>
+              </button>
+            </li>
+          </ul>
         </div>
       </div>
-
-      <table className="table table-striped table-sm mt-3">
-        <thead>
-          <tr>
-            {tableColumn.map((col) => {
-              return <th key={col.id}>{col.columnName}</th>;
-            })}
-          </tr>
-        </thead>
-        <tbody>
-          {tableRows.map((data) => (
-            <tr key={data[keyForTableRows]}>
-              {tableColumn.map((col) => (
-                <td key={col.id}>{renderCell(data, col)}</td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Table;
